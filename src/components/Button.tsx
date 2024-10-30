04e2d8de1a6fa332a6fd8e64bfdf1c3db6d09ad3
heony704 <heony704@gmail.com>
2024-10-30 22:46:41 +0900

    style: 버튼 hover, active 스타일 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ function Button({
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={`${className} flex h-12 flex-shrink-0 cursor-pointer items-center justify-center rounded-lg bg-indigo-500 px-5 text-base font-semibold text-white disabled:cursor-default disabled:bg-gray-400`}
+      className={`${className} hover:bg-primary-600 bg-primary-500 active:bg-primary-700 flex h-12 flex-shrink-0 cursor-pointer items-center justify-center rounded-lg px-5 text-base font-semibold text-white transition-colors disabled:cursor-default disabled:bg-gray-400`}
       {...rest}
     >
       {children}
