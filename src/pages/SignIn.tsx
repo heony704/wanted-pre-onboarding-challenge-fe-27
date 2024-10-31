a70c2d8803ecb0b1ac78ea4b7320c84a83917746
heony704 <heony704@gmail.com>
2024-10-31 18:08:46 +0900

    feat: 로그인/회원가입 페이지에 안내 링크 추가

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 import { signin } from "../api/auth";
@@ -29,45 +29,59 @@ function SignIn() {
   };
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Item id="email" isError={!!errors.email}>
-        <Form.Label>이메일</Form.Label>
-        <Form.Input
-          type="email"
-          placeholder="이메일을 입력해주세요"
-          autoComplete="username"
-          {...register("email", {
-            required: "이메일을 입력해주세요.",
-            pattern: {
-              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
-              message: "잘못된 이메일 형식입니다.",
-            },
-          })}
-        />
-        <Form.ErrorMessage>{errors.email?.message}</Form.ErrorMessage>
-      </Form.Item>
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Item id="email" isError={!!errors.email}>
+          <Form.Label>이메일</Form.Label>
+          <Form.Input
+            type="email"
+            placeholder="이메일을 입력해주세요"
+            autoComplete="username"
+            {...register("email", {
+              required: "이메일을 입력해주세요.",
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+                message: "잘못된 이메일 형식입니다.",
+              },
+            })}
+          />
+          <Form.ErrorMessage>{errors.email?.message}</Form.ErrorMessage>
+        </Form.Item>
 
-      <Form.Item id="password" isError={!!errors.password}>
-        <Form.Label>비밀번호</Form.Label>
-        <Form.Input
-          type="password"
-          placeholder="비밀번호를 입력해주세요"
-          autoComplete="current-password"
-          {...register("password", {
-            required: "비밀번호를 입력해주세요.",
-            minLength: {
-              value: 8,
-              message: "비밀번호를 8자 이상 입력해주세요.",
-            },
-          })}
-        />
-        <Form.ErrorMessage>{errors.password?.message}</Form.ErrorMessage>
-      </Form.Item>
+        <Form.Item id="password" isError={!!errors.password}>
+          <Form.Label>비밀번호</Form.Label>
+          <Form.Input
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            autoComplete="current-password"
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              minLength: {
+                value: 8,
+                message: "비밀번호를 8자 이상 입력해주세요.",
+              },
+            })}
+          />
+          <Form.ErrorMessage>{errors.password?.message}</Form.ErrorMessage>
+        </Form.Item>
 
-      <Button className="mt-2" type="submit" disabled={!isValid}>
-        로그인
-      </Button>
-    </Form>
+        <Button className="mt-2" type="submit" disabled={!isValid}>
+          로그인
+        </Button>
+      </Form>
+
+      <div>
+        <span className="text-sm font-normal text-gray-700">
+          아직 회원이 아니신가요?
+        </span>
+        <Link
+          className="text-primary-600 active:text-primary-800 ml-1 text-sm font-medium hover:underline"
+          to="/signup"
+        >
+          회원가입
+        </Link>
+      </div>
+    </>
   );
 }
 
